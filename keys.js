5a52e17c9672648aecf84b0d79768557fad76140
Camerone Stoney <Cameronestoney@yahoo.com>
2019-06-12 21:25:10 -0700

    WIP var keys not working properly

--- keys.js
+++ keys.js
@@ -4,3 +4,13 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+exports.OMBD = {
+  id:"",
+  secret:""
+};
+
+exports.BandsInTown = {
+  id: "",
+  seceret:""
+}
--- liri.js
+++ liri.js
@@ -1,6 +1,25 @@
 // set any environment variables with the dotenv package
 require("dotenv").config();
 
+var command = process.argv[2];
+var input = process.argv[3];
 // importing the keys file and storing it in a variable
-var keys = require("./keys.js");
+var keys = require("keys.js");
 
+// importing spotify
+var Spotify = require('node-spotify-api');
+
+
+
+
+if(command === "spotify-this-song"){
+    Spotify.search({ type: 'track', query: "'" + input +"'" }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+       
+      console.log(data); 
+      });
+}else{
+    return("error")
+}
