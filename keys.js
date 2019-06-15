49392bf354de7c4dd405a87f00f8879d7b3bcc4c
Camerone Stoney <Cameronestoney@yahoo.com>
2019-06-15 09:47:48 -0700

    omdb key working and request being sent. need to figure out the correct responses

--- keys.js
+++ keys.js
@@ -1,16 +1,10 @@
 console.log('this is loaded');
 
-exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
-};
+// exports.spotify = {
+//   id: process.env.SPOTIFY_ID,
+//   secret: process.env.SPOTIFY_SECRET
+// };
 
-exports.OMBD = {
-  id:"",
-  secret:""
-};
-
-exports.BandsInTown = {
-  id: "",
-  seceret:""
+exports.OMDB = {
+  id: process.env.OMDB_KEY
 }
--- liri.js
+++ liri.js
@@ -1,25 +1,43 @@
 // set any environment variables with the dotenv package
 require("dotenv").config();
 
-var command = process.argv[2];
-var input = process.argv[3];
 // importing the keys file and storing it in a variable
-var keys = require("keys.js");
+var keys = require("./keys");
+const axios = require('axios');
+var movie = keys.OMDB.id
+// var Spotify = require('node-spotify-api');
+var command = process.argv[3];
+var input = process.argv[4];
 
-// importing spotify
-var Spotify = require('node-spotify-api');
 
 
+function getSongInfo() {
+  var spotify = new Spotify(keys.spotify);
 
+  if (command === "spotify-this-song") {
+    spotify.search({ type: 'track', query: "'" + input + "'" }, function (err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
 
-if(command === "spotify-this-song"){
-    Spotify.search({ type: 'track', query: "'" + input +"'" }, function(err, data) {
-        if (err) {
-          return console.log('Error occurred: ' + err);
-        }
-       
-      console.log(data); 
-      });
-}else{
-    return("error")
+      console.log(data);
+    });
+  };
 }
+
+function getMovieInfo() {
+  axios.get("http://www.omdbapi.com/?apikey="+movie+"&t=Batman&page=1")
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .finally(function () {
+      // always executed
+    });
+}
+
+getMovieInfo()
